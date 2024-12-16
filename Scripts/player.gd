ea778d340567596bfd057c77781d71a0abaefca6
João Szlachta <jpbs2002@gmail.com>
2024-12-16 20:03:18 -0300

    fix: set animationTree properly

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
+@onready var animation_tree: AnimationTree = $AnimationTree	
+@onready var animation_state = animation_tree.get("parameters/playback")
 
 @export var speed = 400
-
-#Get the input commands
-func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * speed
+var input_direction = Vector2.ZERO
 
 #Make it moves
 func _physics_process(_delta):
-	get_input()
+	move()
+	
+func move():
+	input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if input_direction != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_direction)
+		animation_tree.set("parameters/Walk/blend_position", input_direction)
+		animation_state.travel("Walk")
+		
+		velocity = input_direction * speed
+	
+	if input_direction == Vector2.ZERO:
+		animation_state.travel("Idle")
+		velocity = Vector2.ZERO
+		
 	move_and_slide()
